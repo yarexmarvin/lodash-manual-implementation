4e3d01f71144763771bbb73ba2be0fba91c8edb4
Yarex Marvin <yarexmarvin@yahoo.com>
2022-03-23 11:27:56 +0400

    flatten deep short way

--- src/tasks/flattenDeep.js
+++ src/tasks/flattenDeep.js
@@ -1,13 +1,19 @@
 
-export default function flattenDeep(arr, result = []) {
+// export default function flattenDeep(arr, result = []) {
 
-  if (Array.isArray(arr)) {
-    for (let element of arr) {
-      flattenDeep(element, result);
-    }
-  } else {
-    return result.push(arr);
-  }
+//   if (Array.isArray(arr)) {
+//     for (let element of arr) {
+//       flattenDeep(element, result);
+//     }
+//   } else {
+//     return result.push(arr);
+//   }
 
-  return result;
-}
+//   return result;
+// }
+
+
+// shorter way
+const flattenDeep = (arr) =>  arr.flatMap(element => Array.isArray(element)? flattenDeep(element): element);
+
+export default flattenDeep
