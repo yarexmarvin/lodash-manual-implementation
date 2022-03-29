61bee508b1361b690d02c70c9002989ce2275d61
Yarex Marvin <yarexmarvin@yahoo.com>
2022-03-29 10:22:47 +0400

    last

---
+++ src/tasks/last.js
@@ -0,0 +1,5 @@
+
+const last = (arr) => arr[arr.length - 1];
+export default last;
+
+
---
+++ src/tests/last.test.js
@@ -0,0 +1,5 @@
+import last from "../tasks/last";
+
+it('last', () => {
+    expect(last([1,2,3])).toBe(3);
+});
